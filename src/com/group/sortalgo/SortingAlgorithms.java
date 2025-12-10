aff8d41745b50c8ef22957174c2248b6a1cb8827
ravishansenevirathna <travishan55@gmail.com>
2025-12-10 08:01:01 +0530

    Add Heap Sort implementation to SortingAlgorithms

--- src/com/group/sortalgo/SortingAlgorithms.java
+++ src/com/group/sortalgo/SortingAlgorithms.java
@@ -114,4 +114,39 @@ public class SortingAlgorithms {
         return i + 1;
     }
 
+    // 5) Heap Sort
+    public static void heapSort(double[] arr) {
+        int n = arr.length;
+
+        // build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // one by one extract elements
+        for (int i = n - 1; i > 0; i--) {
+            double temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(double[] arr, int n, int i) {
+        int largest = i;
+        int left  = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+
+        if (largest != i) {
+            double swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
 }
